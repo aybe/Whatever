6e62300e0fc9af7f7515e886fc0547f535ba6da2
aybe <aybe@users.noreply.github.com>
2023-11-08 11:11:37 +0100

    Fix warnings in dictionary extensions

--- Whatever.Extensions/DictionaryExtensions.cs
+++ Whatever.Extensions/DictionaryExtensions.cs
@@ -1,12 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Whatever.Extensions
 {
     public static class DictionaryExtensions
     {
-        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, Func<TValue> valueFactory)
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, [DisallowNull] TKey key, Func<TValue> valueFactory)
         {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (valueFactory == null)
+            {
+                throw new ArgumentNullException(nameof(valueFactory));
+            }
+
             if (dictionary.TryGetValue(key, out var value))
             {
                 return value;
