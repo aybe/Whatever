04d21c2fc10b252ca2d58e3e94b6ef6aab2de61f
aybe <aybe@users.noreply.github.com>
2024-01-17 04:06:52 +0100

    XA: clone our 8/1 as 8/2

--- Whatever.Interop.Tests/NewFolder/XaDecoder.cs
+++ Whatever.Interop.Tests/NewFolder/XaDecoder.cs
@@ -27,7 +27,7 @@ public static class XaDecoder
         {
             if (isStereo)
             {
-                int i; // TODO
+                output.SampleCount = (uint)Decode82(src, output.Samples, blocks);
             }
             else
             {
@@ -48,6 +48,52 @@ public static class XaDecoder
     }
 
     private static int Decode81(Span<byte> source, Span<short> target, int blocks)
+    {
+        var index = 0;
+
+        ref var older = ref History[0][1];
+        ref var old = ref History[0][0];
+
+        for (var group = 0; group < 18; group++)
+        {
+            Globals.WriteLine($"{nameof(group)}: {group}");
+
+            for (var block = 0; block < blocks; block++)
+            {
+                var sp = source[4 + block];
+                var sr = sp & 0xF;
+                var sf = (sp & 0x30) >> 4;
+                var f0 = Globals.PositiveFilters[sf];
+                var f1 = Globals.NegativeFilters[sf];
+
+                Globals.WriteLine(
+                    $"\t{nameof(block)}: {block}, " +
+                    $"{nameof(sp)}: 0x{sp:X2}, " +
+                    $"{nameof(sr)}: {sr}, " +
+                    $"{nameof(sf)}: {sf}, " +
+                    $"{nameof(f0)}: {f0,3}, " +
+                    $"{nameof(f1)}: {f1,3}"
+                );
+
+                for (var sample = 0; sample < 28; sample++)
+                {
+                    int t = source[16 + block + sample * 4];
+                    t = (t << 24) >> 24;
+                    var s = (t << (8 - sr)) + (old * f0 + older * f1 + 32) / 64;
+                    s = Math.Clamp(s, short.MinValue, short.MaxValue);
+                    older = old;
+                    old = s;
+                    target[index++] = (short)s;
+                }
+            }
+
+            source = source[128..];
+        }
+
+        return index;
+    }
+
+    private static int Decode82(Span<byte> source, Span<short> target, int blocks)
     {
         var index = 0;
 
