cd31c356b6361c368459712d0e6ca3be5b6a8a30
aybe <aybe@users.noreply.github.com>
2024-01-18 10:03:14 +0100

    XA: pass the context to inner method

--- Whatever.Interop.Tests/NewFolder/XaDecoder.cs
+++ Whatever.Interop.Tests/NewFolder/XaDecoder.cs
@@ -23,28 +23,28 @@ public static class XaDecoder
         {
             if (isStereo)
             {
-                ctx.SampleCount = (uint)Decode(src, ctx.Samples, 2, 2, 8, 0, 0, 0xFF, 24, 8);
+                ctx.SampleCount = (uint)Decode(ref ctx, src, ctx.Samples, 2, 2, 8, 0, 0, 0xFF, 24, 8);
             }
             else
             {
-                ctx.SampleCount = (uint)Decode(src, ctx.Samples, 1, 4, 8, 0, 0, 0xFF, 24, 8);
+                ctx.SampleCount = (uint)Decode(ref ctx, src, ctx.Samples, 1, 4, 8, 0, 0, 0xFF, 24, 8);
             }
         }
         else
         {
             if (isStereo)
             {
-                ctx.SampleCount = (uint)Decode(src, ctx.Samples, 2, 4, 4, 0, 1, 0x0F, 28, 12);
+                ctx.SampleCount = (uint)Decode(ref ctx, src, ctx.Samples, 2, 4, 4, 0, 1, 0x0F, 28, 12);
             }
             else
             {
-                ctx.SampleCount = (uint)Decode(src, ctx.Samples, 1, 8, 4, 1, 0, 0x0F, 28, 12);
+                ctx.SampleCount = (uint)Decode(ref ctx, src, ctx.Samples, 1, 8, 4, 1, 0, 0x0F, 28, 12);
             }
         }
     }
 
     private static int Decode(
-        Span<byte> source, Span<short> target, int channels, int blocks, int bits, int blockMask, int channelMask, int sampleMask, int signShift, int sampleShift)
+        ref XaDecoderContext ctx, Span<byte> source, Span<short> target, int channels, int blocks, int bits, int blockMask, int channelMask, int sampleMask, int signShift, int sampleShift)
     {
         var index = 0;
 
