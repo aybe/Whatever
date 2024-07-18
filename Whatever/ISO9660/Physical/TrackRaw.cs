445a5e6952085d3d975630b70d28bb56eefef3ad
aybe <aybe@users.noreply.github.com>
2024-07-18 05:05:31 +0200

    DRY 1st EZ case of async DeviceIoControl

--- Whatever/ISO9660/Physical/TrackRaw.cs
+++ Whatever/ISO9660/Physical/TrackRaw.cs
@@ -1,8 +1,5 @@
-﻿using System.ComponentModel;
-using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
+﻿using System.Runtime.Versioning;
 using Microsoft.Win32.SafeHandles;
-using Whatever.Extensions;
 using Whatever.ISO9660.Extensions;
 
 namespace Whatever.ISO9660.Physical;
@@ -52,100 +49,15 @@ internal sealed class TrackRaw(int index, int position, int length, bool audio, 
     }
 
     [SupportedOSPlatform("windows")]
-    private async Task<ISector> ReadSectorWindowsAsync(int index, uint timeout = 3u)
+    private async Task<ISector> ReadSectorWindowsAsync(int index, uint timeout = 3u) // TODO move to Disc?
     {
-        var query = Disc.ReadSectorWindowsQuery((uint)index, 1u, timeout, Buffer.Pointer, Buffer.Length);
+        using var query = Disc.ReadSectorWindowsQuery((uint)index, 1u, timeout, Buffer.Pointer, Buffer.Length);
 
-        await using var y = query.ConfigureAwait(false);
-
-        var state = new ReadSectorWindowsAsyncState();
-
-        await using var z = state.ConfigureAwait(false);
-
-        var source = new TaskCompletionSource();
-
-        var handle = ReadSectorWindowsAsync(query, state, source, TimeSpan.FromSeconds(timeout));
-
-        if (handle != null)
-        {
-            var error = Marshal.GetLastPInvokeError();
-
-            if (error is not (NativeConstants.ERROR_SUCCESS or NativeConstants.ERROR_IO_PENDING))
-            {
-                throw new Win32Exception(error);
-            }
-
-            await source.Task.ConfigureAwait(false);
-
-            handle.Unregister(null);
-        }
+        await DeviceIoControl.SendAsync(
+            Handle, NativeConstants.IOCTL_SCSI_PASS_THROUGH_DIRECT, query, query, TimeSpan.FromSeconds(timeout));
 
         var sector = ISector.Read(Sector, Buffer.Manager.Memory.Span);
 
         return sector;
-    }
-
-    [SupportedOSPlatform("windows")]
-    private unsafe RegisteredWaitHandle? ReadSectorWindowsAsync(
-        NativeMarshaller<NativeTypes.SCSI_PASS_THROUGH_DIRECT> query,
-        ReadSectorWindowsAsyncState state,
-        TaskCompletionSource source,
-        TimeSpan timeout)
-    {
-        var overlapped =
-            new Overlapped(0, 0, state.Event.SafeWaitHandle.DangerousGetHandle(), null)
-                .Pack(null, null);
-
-        var ioctl = NativeMethods.DeviceIoControl(
-            Handle,
-            NativeConstants.IOCTL_SCSI_PASS_THROUGH_DIRECT,
-            query.Pointer, (uint)query.Length, query.Pointer, (uint)query.Length,
-            out _,
-            overlapped
-        );
-
-        if (ioctl is false)
-        {
-            return ThreadPool.RegisterWaitForSingleObject(state.Event, ReadSectorWindowsAsync, source, timeout, true);
-        }
-
-        Overlapped.Free(overlapped); // implicit when async...
-
-        return null;
-    }
-
-    [SupportedOSPlatform("windows")]
-    private static void ReadSectorWindowsAsync(object? state, bool timedOut)
-    {
-        var source = state as TaskCompletionSource
-                     ?? throw new ArgumentOutOfRangeException(nameof(state));
-
-        try
-        {
-            if (timedOut)
-            {
-                source.SetCanceled();
-            }
-            else
-            {
-                source.SetResult();
-            }
-        }
-        catch (Exception e)
-        {
-            source.SetException(e);
-        }
-    }
-
-    [SupportedOSPlatform("windows")]
-    private sealed class ReadSectorWindowsAsyncState : DisposableAsync
-    // for the magic of 'await using'
-    {
-        public readonly ManualResetEvent Event = new(false);
-
-        protected override void DisposeManaged()
-        {
-            Event.Dispose();
-        }
     }
 }
