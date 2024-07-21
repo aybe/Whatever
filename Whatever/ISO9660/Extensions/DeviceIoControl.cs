d6e0f5bff084cc87dd03e23d8d8d3fd69accc571
aybe <aybe@users.noreply.github.com>
2024-07-21 22:00:45 +0200

    Simplify SendWindowsAsync by moving logic to state

--- Whatever/ISO9660/Extensions/DeviceIoControl.cs
+++ Whatever/ISO9660/Extensions/DeviceIoControl.cs
@@ -10,6 +10,7 @@ namespace Whatever.ISO9660.Extensions;
 
 public static class DeviceIoControl
 {
+    /// <exception cref="Win32Exception" />
     private static void CheckLastError(bool flag)
     {
         if (flag)
@@ -97,114 +98,96 @@ public static class DeviceIoControl
         ArgumentNullException.ThrowIfNull(source);
         ArgumentNullException.ThrowIfNull(target);
 
-        var tcs = new TaskCompletionSource<uint>();
+        WindowsAsyncState state;
 
-        using var @event = new ManualResetEvent(false);
+        using var e = new ManualResetEvent(false);
 
-        using var result = SendWindowsAsync(handle, code, source, target, timeout, tcs, @event);
-
-        if (result.Handle is null)
+        unsafe
         {
-            return result.Length;
-        }
+            var overlapped =
+                new Overlapped(0, 0, e.SafeWaitHandle.DangerousGetHandle(), null)
+                    .Pack(null, null);
 
-        var bytes = await tcs.Task.ConfigureAwait(false);
-
-        return bytes;
-    }
-
-    [SupportedOSPlatform("windows")]
-    private static unsafe Result SendWindowsAsync<TSource, TTarget>(
-        SafeFileHandle handle,
-        uint code,
-        NativeMarshaller<TSource> src,
-        NativeMarshaller<TTarget> tgt,
-        TimeSpan timeout,
-        TaskCompletionSource<uint> tcs,
-        WaitHandle evt
-    )
-        where TSource : struct
-        where TTarget : struct
-    {
-        var overlapped =
-            new Overlapped(0, 0, evt.SafeWaitHandle.DangerousGetHandle(), null)
-                .Pack(null, null);
-
-        var ioctl = NativeMethods.DeviceIoControl(
-            handle, code, src.Pointer, (uint)src.Length, tgt.Pointer, (uint)tgt.Length, out var len, overlapped
-        );
-
-        var rwh = default(RegisteredWaitHandle?);
-
-        if (ioctl)
-        {
-            Overlapped.Free(overlapped);
-        }
-        else
-        {
-            rwh = ThreadPool.RegisterWaitForSingleObject(
-                evt, SendWindowsAsyncCallback, new WindowsState(handle, overlapped, tcs), timeout, true
+            var ioctl = NativeMethods.DeviceIoControl(
+                handle, code,
+                source.Pointer, (uint)source.Length,
+                target.Pointer, (uint)target.Length,
+                out var length, overlapped
             );
+
+            try
+            {
+                CheckLastError(ioctl);
+
+                if (ioctl)
+                {
+                    return length;
+                }
+            }
+            finally
+            {
+                if (ioctl)
+                {
+                    Overlapped.Free(overlapped);
+                }
+            }
+
+            state = new WindowsAsyncState(handle, overlapped, e, timeout);
         }
 
-        return new Result(rwh, len);
+        return await state.Source.Task.ConfigureAwait(false);
     }
 
     [SupportedOSPlatform("windows")]
-    private static unsafe void SendWindowsAsyncCallback(object? state, bool timedOut)
+    private sealed unsafe class WindowsAsyncState
     {
-        if (state is not WindowsState ws)
+        private readonly SafeFileHandle FileHandle;
+
+        private readonly NativeOverlapped* Overlapped;
+
+        private readonly RegisteredWaitHandle WaitHandle;
+
+        public WindowsAsyncState(
+            SafeFileHandle fileHandle, NativeOverlapped* overlapped, WaitHandle waitHandle, TimeSpan timeout)
         {
-            throw new ArgumentOutOfRangeException(nameof(state), state, null);
+            ArgumentNullException.ThrowIfNull(fileHandle);
+            ArgumentNullException.ThrowIfNull(overlapped);
+            ArgumentNullException.ThrowIfNull(waitHandle);
+
+            FileHandle = fileHandle;
+            Overlapped = overlapped;
+            WaitHandle = ThreadPool.RegisterWaitForSingleObject(waitHandle, Callback, this, timeout, true);
         }
 
-        var tcs = ws.Source;
+        public TaskCompletionSource<uint> Source { get; } = new();
 
-        try
+        private void Callback(object? state, bool timedOut)
         {
-            if (timedOut)
+            try
             {
-                tcs.SetCanceled();
-            }
-            else
-            {
-                var result = NativeMethods.GetOverlappedResult(ws.Handle, ws.Overlapped, out var length, true);
+                if (timedOut)
+                {
+                    Source.SetCanceled();
+                }
+                else
+                {
+                    var result = NativeMethods.GetOverlappedResult(FileHandle, Overlapped, out var bytes, true);
 
-                CheckLastError(result);
+                    CheckLastError(result);
 
-                tcs.SetResult(length);
+                    Source.SetResult(bytes);
+                }
             }
-        }
 #pragma warning disable CA1031 // Do not catch general exception types
-        catch (Exception e)
+            catch (Exception e)
 #pragma warning restore CA1031
-        {
-            tcs.SetException(e);
+            {
+                Source.SetException(e);
+            }
+            finally
+            {
+                WaitHandle.Unregister(null);
+            }
         }
-    }
-
-    private readonly struct Result(RegisteredWaitHandle? handle, uint length) : IDisposable
-    {
-        public readonly RegisteredWaitHandle? Handle = handle;
-
-        public readonly uint Length = length;
-
-        public void Dispose()
-        {
-            Handle?.Unregister(null);
-        }
-    }
-
-    [SupportedOSPlatform("windows")]
-    private readonly unsafe struct WindowsState(
-        SafeFileHandle handle,
-        NativeOverlapped* overlapped,
-        TaskCompletionSource<uint> source)
-    {
-        public readonly SafeFileHandle Handle = handle;
-
-        public readonly NativeOverlapped* Overlapped = overlapped;
-
-        public readonly TaskCompletionSource<uint> Source = source;
     }
 }
